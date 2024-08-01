9b0dc143abeec4d1407462c4c3c210901b540ff6
ziheng <shadow1746556951@gmail.com>
2024-08-02 01:17:46 +0800

    feat: eslint & prettierr

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,42 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'react', 'react-hooks'],
   rules: {
-    'react-refresh/only-export-components': [
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'react/prop-types': [0],
+    'react-hooks/exhaustive-deps': [
       'warn',
-      { allowConstantExport: true },
+      {
+        additionalHooks: '(useMyCustomHook|useMyOtherCustomHook)',
+      },
     ],
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
-}
+  settings: {
+    react: {
+      version: '18',
+    },
+  },
+  ignorePatterns: [
+    '**/.eslintrc.cjs',
+    'dist',
+    'pnpm-lock.yaml',
+    'tailwind.config.ts',
+    'node_modules',
+    'postcss.config.js',
+  ],
+};
