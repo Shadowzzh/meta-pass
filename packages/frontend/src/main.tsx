a2b16e768a000c9061367cab2c5e71b1c91c5c14
ziheng <shadow1746556951@gmail.com>
2024-08-12 00:04:11 +0800

    style: 全局样式更改

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
+import { Toaster } from '@/components/ui/toaster';
 import { Web3Provider } from '@/components/Web3Provider';
 import { browserRouter } from '@/router/config';
 
@@ -12,5 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Web3Provider>
       <RouterProvider router={browserRouter} />
     </Web3Provider>
+    <Toaster />
   </React.StrictMode>,
 );
