4f84f5cfed8cc59b32e9b69d56d5ad9b7075797e
ziheng <shadow1746556951@gmail.com>
2024-09-14 10:10:39 +0800

    build: 增加 github 环境

--- packages/frontend/src/vite-env.d.ts
+++ packages/frontend/src/vite-env.d.ts
@@ -1,2 +1,14 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  /**
+   * 构建模式
+   * - github: 用于 GitHub Pages
+   */
+  readonly VITE_BUILD_MODE: 'github';
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
