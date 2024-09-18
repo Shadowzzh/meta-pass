4b70c8832f584fcd7fee55f305416aa3dc0a32dc
ziheng <shadow1746556951@gmail.com>
2024-09-19 00:15:26 +0800

    feat(frontend): 在 github 环境中使用 sepolia 环境合约地址

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -1,6 +1,9 @@
 import CONTRACT from '@contract/artifacts/contracts/TicketSystem.sol/TicketSystem.json';
 
 /** 合约地址 */
-export const CONTRACT_ADDRESS = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+export const CONTRACT_ADDRESS =
+  import.meta.env.VITE_BUILD_MODE === 'github'
+    ? '0x50f91afc1331D876D66F1e26C35078605d2450e5'
+    : '0x5fbdb2315678afecb367f032d93f642f64180aa3';
 /** 合约 abi */
 export const ABI = CONTRACT.abi;
